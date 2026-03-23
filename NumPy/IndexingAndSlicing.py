d42eb621db9caaca68a6e4dc8f854563282b1270
Prashanta <prashantkumarbehera1@gmail.com>
2026-03-23 17:21:27 +0530

    Indexing and slicing

---
+++ NumPy/IndexingAndSlicing.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#Indexing
+arr= np.array([1,2,3])
+print(arr[2])
+
+#Slicing
+arr= np.array([1,2,3,4,5,6])
+print(arr[0:3])
+print(arr[2:5])
+
+#2D indexing
+arr = np.array([[1,2,3],[4,5,6]])
+print(arr[0,1])
---
+++ NumPy/MathematicalOperations.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+
+print(a+b)
+print(a*b)
+
+print(np.mean(a))
+print(np.median(a))
+print(np.std(a))
+print(np.min(a))
+print(np.max(a))
