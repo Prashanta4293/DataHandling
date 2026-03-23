e6c9783ce5cd950129177d8ca7f785ce50d41f74
Prashanta <prashantkumarbehera1@gmail.com>
2026-03-23 15:05:31 +0530

    new files for data handling NumPy

---
+++ NumPy/MultiDimensionalArray.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.array([[[[[[1,2],[2,3],[3,4]]]]]])
+print(arr)
+
+print(arr.shape) #dimensions(rows, columns)
+print(arr.ndim) #number of dimensions
+print(arr.dtype) #data type
+print(arr.size) #total elements
+
